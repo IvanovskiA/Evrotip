5d086fe67c18ddfae86006ae8c9c6542e4eb4c46
IvanovskiA <ivanovskiivanovski5@gmail.com>
2022-08-08 14:39:12 +0200

    check uploaded file/s in takeData function

--- functions/write_functions.php
+++ functions/write_functions.php
@@ -26,42 +26,46 @@ function takeData($fileCount, $acceptedext)
   for ($i = 0; $i < $fileCount; $i++) {
     $fileName = $_FILES['file']['name'][$i];
     $fileTmpName = $_FILES['file']['tmp_name'][$i];
-    $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-    if (!in_array($extension, $acceptedext)) {
-      $message .= " Wrong file " . $fileName;
+    if (!file_exists($fileTmpName) || !is_uploaded_file($fileTmpName)) {
+      $message .= 'No uploaded file/s';
     } else {
-      $dom->Load($fileTmpName);
+      $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+      if (!in_array($extension, $acceptedext)) {
+        $message .= " Wrong file " . $fileName;
+      } else {
+        $dom->Load($fileTmpName);
 
-      $referenceNo = $dom->getElementsByTagName('ReferenceNo')->item(0)->nodeValue;
-      $dateCreated = $dom->getElementsByTagName('DateCreated')->item(0)->nodeValue;
-      $dataFromDate = $dom->getElementsByTagName('DataFromDate')->item(0)->nodeValue;
-      $dataToDate = $dom->getElementsByTagName('DataFromDate')->item(0)->nodeValue;
-      $dateCreatedPreg = preg_replace("/[A-Za-z]/", " ", $dateCreated);
-      $dataFromDatePreg = preg_replace("/[A-Za-z]/", " ", $dataFromDate);
-      $dataToDatePreg = preg_replace("/[A-Za-z]/", " ", $dataToDate);
-      $transactionDate = $dom->getElementsByTagName('TransactionDate')->item(0)->nodeValue;
-      $personList = $dom->getElementsByTagName('Person');
-      foreach ($personList as $person) {
-        $personObjectId = $person->getElementsByTagName('PersonObjectId')->item(0)->nodeValue;
-        $isResident = $person->getElementsByTagName('IsResident')->item(0)->nodeValue;
-        $firstName = $person->getElementsByTagName('FirstName')->item(0)->nodeValue;
-        $genderTypeId = $person->getElementsByTagName('GenderTypeId')->item(0)->nodeValue;
-        $lastName = $person->getElementsByTagName('LastName')->item(0)->nodeValue;
-        $idDocumentTypeId = $person->getElementsByTagName('IdDocumentTypeId')->item(0)->nodeValue;
-        $idNo = $person->getElementsByTagName('IdNo')->item(0)->nodeValue;
-        $addressTypeId = $person->getElementsByTagName('AddressTypeId')->item(0)->nodeValue;
-        $addressLine1 = $person->getElementsByTagName('AddressLine1')->item(0)->nodeValue;
-        $city = $person->getElementsByTagName('City')->item(0)->nodeValue;
-        $isoType = $person->getElementsByTagName('ISOType')->item(0)->nodeValue;
-        $isoCode = $person->getElementsByTagName('ISOCode')->item(0)->nodeValue;
+        $referenceNo = $dom->getElementsByTagName('ReferenceNo')->item(0)->nodeValue;
+        $dateCreated = $dom->getElementsByTagName('DateCreated')->item(0)->nodeValue;
+        $dataFromDate = $dom->getElementsByTagName('DataFromDate')->item(0)->nodeValue;
+        $dataToDate = $dom->getElementsByTagName('DataFromDate')->item(0)->nodeValue;
+        $dateCreatedPreg = preg_replace("/[A-Za-z]/", " ", $dateCreated);
+        $dataFromDatePreg = preg_replace("/[A-Za-z]/", " ", $dataFromDate);
+        $dataToDatePreg = preg_replace("/[A-Za-z]/", " ", $dataToDate);
+        $transactionDate = $dom->getElementsByTagName('TransactionDate')->item(0)->nodeValue;
+        $personList = $dom->getElementsByTagName('Person');
+        foreach ($personList as $person) {
+          $personObjectId = $person->getElementsByTagName('PersonObjectId')->item(0)->nodeValue;
+          $isResident = $person->getElementsByTagName('IsResident')->item(0)->nodeValue;
+          $firstName = $person->getElementsByTagName('FirstName')->item(0)->nodeValue;
+          $genderTypeId = $person->getElementsByTagName('GenderTypeId')->item(0)->nodeValue;
+          $lastName = $person->getElementsByTagName('LastName')->item(0)->nodeValue;
+          $idDocumentTypeId = $person->getElementsByTagName('IdDocumentTypeId')->item(0)->nodeValue;
+          $idNo = $person->getElementsByTagName('IdNo')->item(0)->nodeValue;
+          $addressTypeId = $person->getElementsByTagName('AddressTypeId')->item(0)->nodeValue;
+          $addressLine1 = $person->getElementsByTagName('AddressLine1')->item(0)->nodeValue;
+          $city = $person->getElementsByTagName('City')->item(0)->nodeValue;
+          $isoType = $person->getElementsByTagName('ISOType')->item(0)->nodeValue;
+          $isoCode = $person->getElementsByTagName('ISOCode')->item(0)->nodeValue;
 
-        checkingXmlStructure($referenceNo, $dateCreated, $dataFromDate, $dataToDate, $dateCreatedPreg, $dataFromDatePreg, $dataToDatePreg, $transactionDate, $personObjectId, $isResident, $firstName, $genderTypeId, $lastName, $idDocumentTypeId, $idNo, $addressTypeId, $addressLine1, $city, $isoType, $isoCode);
-        if (!checkingXmlStructure($referenceNo, $dateCreated, $dataFromDate, $dataToDate, $dateCreatedPreg, $dataFromDatePreg, $dataToDatePreg, $transactionDate, $personObjectId, $isResident, $firstName, $genderTypeId, $lastName, $idDocumentTypeId, $idNo, $addressTypeId, $addressLine1, $city, $isoType, $isoCode)) {
-          $message .= $fileName . " Element exist!";
-          continue (2);
+          checkingXmlStructure($referenceNo, $dateCreated, $dataFromDate, $dataToDate, $dateCreatedPreg, $dataFromDatePreg, $dataToDatePreg, $transactionDate, $personObjectId, $isResident, $firstName, $genderTypeId, $lastName, $idDocumentTypeId, $idNo, $addressTypeId, $addressLine1, $city, $isoType, $isoCode);
+          if (!checkingXmlStructure($referenceNo, $dateCreated, $dataFromDate, $dataToDate, $dateCreatedPreg, $dataFromDatePreg, $dataToDatePreg, $transactionDate, $personObjectId, $isResident, $firstName, $genderTypeId, $lastName, $idDocumentTypeId, $idNo, $addressTypeId, $addressLine1, $city, $isoType, $isoCode)) {
+            $message .= $fileName . " Element exist!";
+            continue (2);
+          }
         }
+        folderExists($fileName, $fileTmpName);
       }
-      folderExists($fileName, $fileTmpName);
     }
   }
 }
@@ -84,8 +88,7 @@ function checkingXmlStructure($referenceNo, $dateCreated, $dataFromDate, $dataTo
   }
 }
 
-// move files in folder or creating folder if exist and move in folder 
-// function folderExists($mydir, $fileName, $fileTmpName)
+// move files in folder or creating folder if exist and move in created folder 
 function folderExists($fileName, $fileTmpName)
 {
   global $mydir;
