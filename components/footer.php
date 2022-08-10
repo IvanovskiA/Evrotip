0493f09df9742136445f8de672f4e159561640bf
IvanovskiA <ivanovskiivanovski5@gmail.com>
2022-08-10 14:49:22 +0200

    Added database close tag

--- components/footer.php
+++ components/footer.php
@@ -26,10 +26,7 @@ dynamicTitleAndPath();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?php echo $path . "assets/js/index.js" ?>"></script>
 <script type="text/javascript" src="<?php echo $path . "assets/js/jquery.js" ?>"></script>
-
-
-
-
 </body>
 
 </html>
+<?php mysqli_close($connection); ?>
